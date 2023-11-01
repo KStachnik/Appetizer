12364d0fa18a53080e6dc4d5153c3be4f12a2aff
KStachnik <stachnikrystian@gmail.com>
2023-11-01 22:52:45 +0100

    add AppetizerListView

---
+++ Appetizer/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizer
+//
+//  Created by Krystian Stachnik on 01/11/2023.
+//
+
+import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    let id: Int
+    let name : String
+    let description : String
+    let price : Double
+    let imageURL : String
+    let calories : Int
+    let protein : Int
+    let carbs : Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "Desription for Appetizer. Pretty yummy.",
+                                           price: 9.98,
+                                           imageURL: "",
+                                           calories: 103,
+                                           protein: 64,
+                                           carbs:  81)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
