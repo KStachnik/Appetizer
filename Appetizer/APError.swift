0e1f8d2cdf7005295a2584016389adbd1867936f
KStachnik <stachnikrystian@gmail.com>
2023-11-02 21:46:10 +0100

    add NetworkManager

---
+++ Appetizer/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizer
+//
+//  Created by Krystian Stachnik on 02/11/2023.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
